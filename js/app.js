6370755712826930389b26663ac4099c159b1efd
Micky240300 <michymurtari2000@msn.com>
2019-08-13 10:46:02 -0300

    Challenge Local_Storage

---
+++ js/app.js
@@ -0,0 +1,37 @@
+
+// guardar_localStorage();
+// obtener_localStorage();
+
+// function guardar_localStorage(){
+
+//     let persona = {
+
+//         nombre: "Michele",
+//         edad: 19,
+//         email: "micky@gmail.com"
+
+//     };
+ 
+
+
+// localStorage.setItem("persona", JSON.stringify(persona));
+
+// }
+
+// function obtener_localStorage(){
+
+//     if(localStorage.getItem("persona")){
+
+//     let persona = JSON.parse(localStorage.getItem("persona"));
+
+//     console.log(persona);
+
+//     }
+    
+//     else{
+
+//         console.log("No hay entradas");
+
+//     }
+
+// }
