cacd7fa35fe29333143b50374b9deb87d230683d
Micky240300 <michymurtari2000@msn.com>
2019-08-16 11:31:04 -0300

    Mejora del Challenge 1

---
+++ CRUD LocalStorage JS Vanilla/js/app.js
@@ -0,0 +1,125 @@
+// Variables
+
+
+const formularioUI = document.querySelector('#formulario');
+const listaActividadesUI = document.getElementById('listaActividades');
+let arrayActividades = [];
+
+// Funciones
+
+const CrearItem = (actividad) => {
+
+    let item = {
+        actividad: actividad,
+        estado: false
+    }
+
+    arrayActividades.push(item);
+
+    return item;
+
+}
+
+const GuardarDB = () => {
+
+    localStorage.setItem('rutina', JSON.stringify(arrayActividades));
+    PintarDB();
+}
+
+const PintarDB = () => {
+
+    listaActividadesUI.innerHTML = '';
+
+    arrayActividades = JSON.parse(localStorage.getItem('rutina'));
+
+    if (arrayActividades === null) {
+        arrayActividades = [];
+
+    } else {
+
+        arrayActividades.forEach(element => {
+
+            if (element.estado) {
+                listaActividadesUI.innerHTML += `<div class="alert alert-success" role="alert"> <i class="material-icons float-left mr-2">accessibility</i><b>${element.actividad}</b> - ${element.estado}<span class="float-right"><i class="material-icons">done</i><i class="material-icons">delete</i></span></div></div>`
+
+                
+            }else{
+                listaActividadesUI.innerHTML += `<div class="alert alert-danger" role="alert"> <i class="material-icons float-left mr-2">accessibility</i><b>${element.actividad}</b> - ${element.estado}<span class="float-right"><i class="material-icons">done</i><i class="material-icons">delete</i></span></div></div>`
+
+
+            }
+
+        });
+    }
+
+}
+
+const EliminarDB = (actividad) => {
+    let indexArray;
+        arrayActividades.forEach((element, index) => {
+       
+        if(element.actividad ===actividad) {
+            indexArray = index;   
+       }
+    });
+
+    arrayActividades.splice(indexArray, 1);
+    GuardarDB();
+}
+
+    const EditarDB = (actividad) => {
+        let indexArray = arrayActividades.findIndex((element) => element.actividad === actividad);
+
+        arrayActividades[indexArray].estado = true;
+
+        GuardarDB();
+    }
+
+// EventListener
+
+        formularioUI.addEventListener('submit', (e) => {
+
+            e.preventDefault();
+            let actividadUI = document.querySelector('#actividad').value;
+
+        CrearItem(actividadUI);
+        GuardarDB();
+
+        formularioUI.reset();
+
+        });
+
+        document.addEventListener('DOMContentLoaded', PintarDB);
+
+
+            listaActividadesUI.addEventListener('click', (e) => {
+
+                e.preventDefault();
+
+
+                if (e.target.innerHTML === 'done' || e.target.innerHTML === 'delete') {
+                
+                    let texto = e.path[2].childNodes[2].innerHTML;
+
+                    if (e.target.innerHTML === 'delete') {
+                        // Accion de eliminar
+                        EliminarDB(texto);
+                    }
+
+                    if (e.target.innerHTML === 'done') {
+                        // Accion de editar        
+                        EditarDB(texto);
+                    }
+
+                }
+            });
+
+
+
+
+
+
+
+
+
+
