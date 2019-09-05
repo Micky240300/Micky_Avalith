02a54fc6ae6d55172ca22127aa93219bc7246180
Micky240300 <michymurtari2000@msn.com>
2019-09-05 12:45:14 -0300

    todo app

--- learning-react/practical-react/src/App.js
+++ learning-react/practical-react/src/App.js
@@ -1,24 +1,64 @@
-import React, {useState} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation';
 
 import { todos } from './todos.json';
-console.log(todos);
 
-class App extends Component {
-render() {
+import TodoForm from './components/TodoForm';
 
 
+class App extends Component  {
+  constructor(){
+    super();
+    this.state = {
+      todos
+    };
+    this.handleAddTodo = this.handleAddTodo.bind(this);
+  }
+
+  handleAddTodo(todo) {
+    this.setState({
+      todos: [...this.state.todos, todo]
+    })
+  }
 
 
-  return (
-    <div className="App">
-      <Navigation title="Menu"/>
-      <Navigation title="OMG"/> 
-      
+  render() {
+    const todos = this.state.todos.map((todo, i) => {
+      return(
+      <div className="col-md-4">  
+        <div className="card mt-4">
+          <div className="card-header">
+              <h3>{todo.title}</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+                  {todo.priority}
+              </span>
+          </div>
+              <div className="card-body">
+                <p>{todo.description}</p>
+                <p>{todo.responsible}</p>
+              </div>
+          
+          
+        </div>
+      </div>
+      )
+    })
 
-      
-    </div>
+    return (
+      <div className="App">
+        <Navigation title="Todos"></Navigation>
+            <span className="badge badge-pill badge-light ml-2">
+                { this.state.todos.length }
+              </span>
+        <div className="container">
+          <TodoForm onAddTodo={ this.handleAddTodo }/>
+    
+            <div className="row mt-4">
+              { todos }
+            </div>
+        </div>
+      </div>
     );
   }
 }
