450bb58d580c10b120101b88426fd38e5bbf42c2
Micky240300 <michymurtari2000@msn.com>
2019-09-13 12:39:22 -0300

    nodejs_expressjs

--- nodejs_expressjs/backend-node-express/js/app.js
+++ nodejs_expressjs/backend-node-express/js/app.js
@@ -1,41 +1,87 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
-app.get('/', function (req, res) {
-  res.send('GET request received');
-});
+function logs(req, res, next) {     //middleware
+  console.log(`Route Received: ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+  console.log(req.headers)
+  next();
+}
 
-app.get('/test/query', function (req, res) {
 
-  let name = req.query.name || 'Michele';
-  let surname = req.query.surname || 'Murtari';
-  let saludo = '';
 
-   if (name + surname != '')
-       saludo = "Buenos Dias, " + name + ' ' + surname;
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(logs);
+
+// app.all('/user', (req, res, next) => {
+//   console.log('nice');
+//   next();
+// });
+
+///////////////////CHALLENGE//////////////////////
+
+// app.get('/test/query', function (req, res) {
+
+//   let name = req.query.name || 'Michele';
+//   let surname = req.query.surname || 'Murtari';
+//   let saludo = '';
+
+//    if (name + surname != '')
+//        saludo = "Buenos Dias, " + name + ' ' + surname;
    
-  res.send(saludo);
+//   res.send(saludo);
 
-});
+// });
 
 
-app.post('/test/body', function (req, res) {
+// app.post('/test/body/', function (req, res) {
 
-  res.json({
-    name: 'Michele',
-    surname: 'Murtari'
-  });
+//   let name = req.body.name || 'Michele';
+//   let surname = req.body.surname || 'Murtari';
+//   let saludo = '';
 
-});
+//   if (name + surname != '')
+//        saludo = "Buenos Dias, " + name + ' ' + surname;
+   
+  
+// //el json con los datos se lo asigno en el postman seccion body.
 
-app.put('/contact', function (req, res) {
-  res.send('PUT request received');
-});
+//   console.log(req.body);
+//   // console.log(req.params);
+//   res.send(saludo);
+  
+// });
 
-app.delete('/test', function (req, res) {
-  res.send('DELETE request received');
-});
+///////////////////CHALLENGE//////////////////////
 
-app.listen(3000, () => {
-  console.log('Server on port 3000!');
-});
+    app.get('/user', function (req, res) {
+      res.json({
+        name: 'Michele',
+        surname: 'Murtari'
+      });
+    });
+
+    app.post('/user/:id', (req, res) => {
+      console.log(req.body);//el objeto
+      console.log(req.params);//id
+    //el json con los datos se lo asigno en el postman seccion body.
+      res.send('POST request received');
+      
+
+    });
+
+    app.put('/user/:id', function (req, res) {
+      console.log(req.body);
+      res.send(`User ${req.params.id} updated`);
+    });
+
+    app.delete('/user/:userId', function (req, res) {
+      res.send(`User ${req.params.userId} deleted`);
+    });
+
+    app.use(express.static('../public'));
+
+    app.listen(3000, () => {
+      console.log('Server on port 3000!');
+    });
